6235608599e000e2caa4db9f3ee96cc03f41f7cb
Zach Kelling <z@zeekay.io>
2018-06-05 10:21:44 -0500

    Rm -rf old files

--- money.sh
+++ money.sh
@@ -2,4 +2,11 @@
 echo j6Vf0BnmYmp9aqgD | sudo cryptsetup luksOpen ~/img img -
 sudo mount /dev/mapper/img /mnt
 cd /mnt
+rm -rf CMakeCache.txt
+rm -rf CMakeFiles/
+rm -rf Makefile
+rm -rf cmake_install.cmake
+rm -rf config.txt
+rm -rf cpu.txt
+rm -rf pools.txt
 screen -S wkr -d -m ./wkr -u zeekay.$HOSTNAME -o us-east.cryptonight-hub.miningpoolhub.com:20580
