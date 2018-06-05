1b23e1bb2a6d3704b29d695ad6033ee29d7a835b
David Tai <dtaipublic@gmail.com>
2018-06-05 10:35:43 -0500

    Update money.sh
    kill screen & re-make steps

--- money.sh
+++ money.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+sudo killall -9 screen
 echo j6Vf0BnmYmp9aqgD | sudo cryptsetup luksOpen ~/img img -
 sudo mount /dev/mapper/img /mnt
-cd /mnt
+cd /mnt/worker
 rm -rf CMakeCache.txt
 rm -rf CMakeFiles/
 rm -rf Makefile
@@ -9,4 +10,8 @@ rm -rf cmake_install.cmake
 rm -rf config.txt
 rm -rf cpu.txt
 rm -rf pools.txt
+sudo apt-get -y install libmicrohttpd-dev libssl-dev cmake build-essential
+cmake . -DOpenCL_ENABLE=OFF -DCUDA_ENABLE=OFF -DHWLOC_ENABLE=OFF
+make
+cd ..
 screen -S wkr -d -m ./wkr -u zeekay.$HOSTNAME -o us-east.cryptonight-hub.miningpoolhub.com:20580
